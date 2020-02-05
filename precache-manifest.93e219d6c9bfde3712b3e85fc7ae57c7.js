4d419ace40855a058a4df551647b38ff57afd76f
rtxrulez <>
2020-02-05 14:49:43 +0300

    Updates

---
+++ precache-manifest.93e219d6c9bfde3712b3e85fc7ae57c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c5adea9b0c7174d3f501a35f60cb95",
+    "url": "/login-app/index.html"
+  },
+  {
+    "revision": "80e485c19ddcdba458a4",
+    "url": "/login-app/static/css/main.fa26d0a2.chunk.css"
+  },
+  {
+    "revision": "06db7b1739f86980bfc6",
+    "url": "/login-app/static/js/2.7397cd93.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/login-app/static/js/2.7397cd93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e485c19ddcdba458a4",
+    "url": "/login-app/static/js/main.55baa04f.chunk.js"
+  },
+  {
+    "revision": "09d4e8348dd9f37c8442",
+    "url": "/login-app/static/js/runtime-main.3d8efe94.js"
+  }
+]);
