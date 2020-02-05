294755e75a540b194a0086dec76d95218f47a348
rtxrulez <>
2020-02-05 11:28:17 +0300

    Оптимизация кода

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import "antd/dist/antd.css";
 import LoginPageContainer from "./components/LoginPage/LoginPageContainer";
